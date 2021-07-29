ef45d5f574846c59856f337e14e14dba228b17e1
Nadia Harerimana <harern01@li03c02.chimera.hpc.mssm.edu>
2021-07-29 08:22:14 -0400

    made changes on rules

--- workflow/scripts/admixtureplot.R
+++ workflow/scripts/admixtureplot.R
@@ -9,36 +9,78 @@ Qdat = snakemake@input[["Qdat"]]
 
 
 ## Outfile
-kplot.out = snakemake@output[["kplot"]] # Output
+# p1.out = snakemake@output[["p1"]] # Output
+p2.out = snakemake@output[["p2"]] # Output
 
 message(
   "\ninput: ", Qdat,
-  "\noutput:", kplot.out , "\n"
+  "\noutput:" ,p2.out, "\n"
 )
-
+message ("Loading packages")
+library(dplyr)
+library(tibble)
+library(purrr)
+library(forcats)
+library(ggplot2)
+library(ggthemes)
+library(patchwork)
+library(readr)
+library(tidyr)
+library(RColorBrewer)
 
 message("Reading admixture file  \n")
-Q.dat <- read_table2(Qdat)
-Q.dat_long <- Q.dat %>%
-filter(pop == 'sample') %>%
-  pivot_longer(c(K1,K2,K3,K4, K5), names_to = "K", values_to = "prop" ) %>%
-   mutate(K = fct_relevel(K, c("K4","K1","K2","K3","K5"))) %>%
-    arrange(K, prop) %>%
-     mutate(iid = fct_inorder(iid))
+Q.dat <- read_tsv(Qdat)
+Q.dat
 
-p1 <- ggplot(Q.dat_long , aes(x = iid, y = prop, fill = K)) +
+# Q.dat_long <- Q.dat %>%
+# pivot_longer(c(K1,K2,K3,K4, K5), names_to = "K", values_to = "prop" ) %>%
+# mutate(K = fct_relevel(K, c("K4","K1","K2","K3", "K5"))) %>%
+#     arrange(K, prop) %>%
+#      mutate(iid = fct_inorder(iid))
+#
+# p1 <- ggplot(Q.dat_long , aes(x = iid, y = prop, fill = K)) +
+#   geom_bar(position="fill", stat="identity", width = 1) +
+#   scale_fill_brewer(palette = "Set1",
+#                     name="Super Population") +
+#                     breaks=c("K1","K5","K3","K4","K2") ,
+#                     labels=c("AFR", "AMR","EAS", "EUR", "SAS")) +
+#   theme_classic() + labs(x = "Indivuals", y = "Global Ancestry", color ="Super Population") +
+#   facet_grid(~fct_inorder(super_pop), switch = "x", scales = "free", space = "free")+
+#   theme(
+#   axis.text.x = element_blank(),
+#   axis.ticks.x=element_blank(),
+#   axis.title.y=element_blank(),
+#   axis.title.x =element_blank(),
+#   panel.grid.major.x = element_blank())
+#      png(kplot.out, units="in", width=10, height=6, res = 300)
+#      kplot
+#      dev.off()
+
+Q.dat_long2 <- Q.dat %>%
+  pivot_longer(c(AFR,AMR,EAS,SAS,EUR), names_to = "K", values_to = "prop" ) %>%
+  mutate(K = fct_relevel(K, c("EUR","AFR","AMR","EAS","SAS"))) %>%
+  arrange(K, prop) %>%
+  mutate(iid = fct_inorder(iid))
+Q.dat_long2
+
+p2 <- ggplot(Q.dat_long2 , aes(x = iid, y = prop, fill = K)) +
   geom_bar(position="fill", stat="identity", width = 1) +
   scale_fill_brewer(palette = "Set1",
-                      name="Super Population",
-                      breaks=c("K1","K5","K3","K4","K2") ,
-                      labels=c("AFR", "AMR","EAS", "EUR", "SAS")) +
+                      name = "Super Population") +
+                      #breaks=c("K1","K5","K3","K4","K2") ,
+                      #labels=c("AFR", "AMR","EAS", "EUR", "SAS")) +
   theme_classic() + labs(x = "Indivuals", y = "Global Ancestry", color ="Super Population") +
-  facet_grid(~fct_inorder(super_pop), switch = "x", scales = "free", space = "free")+
+  facet_grid(~fct_inorder(super_pop2), switch = "x", scales = "free", space = "free")+
   theme(
         axis.text.x = element_blank(),
          axis.ticks.x=element_blank(),
          axis.title.y=element_blank(),
          axis.title.x =element_blank(),
          panel.grid.major.x = element_blank())
+# png(p1.out, units="in", width=10, height=6, res = 300)
+# p1
+# dev.off()
 
-ggsave(kplot.out, plot = p1, width = 12, height = 6, units = "in")
+png(p2.out, units="in", width=10, height=6, res = 300)
+p2
+dev.off()
--- workflow/scripts/ancestry_assign.R
+++ workflow/scripts/ancestry_assign.R
@@ -5,25 +5,33 @@
 
 
 ## Infiles
-Qfile = snakemake@input[["Qfile"]]
-pops = snakemake@input[["pops"]]
+infile = snakemake@input[[1]] # infile
 
 ## Outfile
-output = snakemake@output[["output"]] # Output
+outfile = snakemake@output[[1]] # Output
 
 message(
-  "\ninput: ", Qfile,pops,
+  "\ninput: ", infile,
   "\noutput:", outfile , "\n"
 )
 
 message ("Loading packages")
 library(dplyr)
-library(tibble
 library(readr)
 library(tidyverse)
 
-message("Reading admixture Qfile  \n")
-Qfile <- read_table2(Qfile) %>%
+message("Reading admixture Qfile \n")
+Qraw <- read_table2(infile)
+Qfile <- Qraw %>%
+  select(c(fid,iid,super_pop, pop, K1, K2, K3, K4, K5)) %>%
+    filter(pop == 'sample') %>%
+      rename(EUR = K4, AFR = K2, SAS = K3, AMR = K1, EAS= K5)%>%
+        mutate(super_pop2 = case_when((EUR > 0.85 & EAS < 0.1 & SAS < 0.1 & AFR <0.1 & AMR <0.1) ~ "EUR",
+                                (EAS > 0.6) ~ "EAS",
+                                (SAS > 0.6) ~ "SAS",
+                                (AFR > 0.3 & EAS < 0.1 & SAS <0.1 & AFR > AMR) ~ "AFR",
+                                (AMR > 0.1 & EAS < 0.1 & SAS <0.1 ) ~ "AMR",
+                                TRUE~"Other"))
 
-message("Reading ancestry population file  \n")
-pops <- read_tsv(pops) %>%
+message("Exporting Qfile to ", outfile, "\n")
+Qfile %>% write_tsv(outfile)
