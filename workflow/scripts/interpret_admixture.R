9b12cffa6f3876a4ea7f40df3cfa09fe07eae18f
sjfandrews <s.j.f.andrews@gmail.com>
2021-08-09 12:51:29 -0400

    fixed sample join issue

--- workflow/scripts/interpret_admixture.R
+++ workflow/scripts/interpret_admixture.R
@@ -6,44 +6,79 @@
 ## Infiles
 Qproj = snakemake@input[["Qproj"]]
 Qsuper = snakemake@input[["Qsuper"]]
-famfile = snakemake@input[["fam"]]
-popfile = snakemake@input[["pops"]]
-pcs = snakemake@input[["pcs"]]
+famfile.path = snakemake@input[["fam"]]
+popfile.path = snakemake@input[["pops"]]
+pcs.path = snakemake@input[["pcs"]]
 
 ## Outfile
 # projected_interpret = snakemake@output[["projected_interpret"]] # Output
 # projected_assign = snakemake@output[["projected_assign"]]
 
-supervised_interpret= snakemake@output[["supervised_interpret"]] # Output
+# supervised_interpret = snakemake@output[["supervised_interpret"]] # Output
 supervised_assign = snakemake@output[["supervised_assign"]]
 
 library(dplyr)
 library(readr)
 library(tidyr)
 
-# Interpreting projected admixture output #
+# Fam and popfiles 
 ## ======================================##
+message("Reading pop file \n")
+popfile <- read_table(popfile.path, col_names = FALSE) %>% 
+  rename(super_pop = X1)
 
-message(" Reading projected admixture output \n")
-tbl_proj <- read_table(Qproj,col_names = FALSE)
+message("Reading fam fixed file \n")
+famfile <- read_table2(famfile.path, col_names = FALSE) %>%
+  rename(fid = X1, iid = X2) %>%
+  bind_cols(popfile) %>% 
+  select(fid, iid, super_pop)
 
 message("Reading pcs file \n")
-  pcs <- read_tsv(pcs)%>%
-  filter(pop == "sample")
-  # fids <- pcs$fid
-  # ids <- pcs$iid
-  # super_pop <- pcs$super_pop
-#
-# message("Calculate the column mean, and assign the column with the max mean the ID \n")
-# to_query <- tbl_proj %>%
-#   mutate(pop = pcs$super_pop)
-#
-# message("Assign column with max mean to that group \n")
-#
-# # Get means for each pop, assign column with max mean to that group
+pcs <- read_tsv(pcs.path) %>% 
+  rename(pca_super_pop = super_pop)
+
+# Interpreting projected admixture output #
+## ======================================##
+message(" Reading projected admixture output \n")
+tbl_proj.raw <- read_table(Qproj,col_names = FALSE)
+
+message("Calculate the column mean, and assign the column with the max mean the ID \n")
+tbl_proj <- tbl_proj.raw %>%
+  bind_cols(filter(famfile, super_pop == "-") %>% select(-super_pop)) %>% 
+  left_join(pcs %>% select(fid, iid, pca_super_pop), by = c('fid', 'iid')) 
+
+message("Assign column with max mean to that group \n")
+
+mean_proj <- tbl_proj %>% 
+  group_by(pca_super_pop) %>% 
+  summarise(k1 = mean(X1), 
+            k2 = mean(X2), 
+            k3 = mean(X3), 
+            k4 = mean(X4), 
+            k5 = mean(X5))
+
+proj_labels <- mean_proj %>% select(starts_with("k")) %>%
+  map_chr(., function(x){
+    slice(mean_proj, which.max(x)) %>% pull(pca_super_pop)
+  }) 
+ 
+out_proj <- tbl_proj %>% 
+  magrittr::set_names(c(proj_labels, 'fid', 'iid', 'pca_super_pop'))  %>% 
+  relocate(fid, iid) %>%
+  mutate(admixture_super_pop = case_when((EUR > 0.85 & EAS < 0.1 & SAS < 0.1 & AFR <0.1 & AMR <0.1) ~ "EUR",
+                                         (EAS > 0.51) ~ "EAS",
+                                         (SAS > 0.51) ~ "SAS",
+                                         (AFR > 0.3 & EAS < 0.1 & SAS <0.1 & AFR > AMR) ~ "AFR",
+                                         (AMR > 0.1 & EAS < 0.1 & SAS <0.1 ) ~ "AMR",
+                                         TRUE~"Other")) 
+  
+# out_proj %>% 
+#   tabyl(admixture_super_pop, pca_super_pop)
+
+# Get means for each pop, assign column with max mean to that group
 # labels=rep(0,ncol(tbl_proj))
-# for (my_pop in unique(to_query$pop)){
-#   subset=to_query[to_query$pop==my_pop,]
+# for (my_pop in unique(to_query$pca_super_pop)){
+#   subset=to_query[to_query$pca_super_pop==my_pop,]
 #   means=apply(subset[,colnames(tbl_proj)],2,mean)
 #   print(means)
 #   ind=which(means==max(means))
@@ -52,10 +87,10 @@ message("Reading pcs file \n")
 # colnames(tbl_proj)=labels
 # tbl_proj <- tbl_proj %>%
 # mutate(fid = pcs$fid, iid =pcs$iid, super_pop = pcs$super_pop)
-#
+# 
 # message("Exporting tbl to", interpret, "\n")
 # tbl_proj %>% write_tsv(projected_interpret)
-#
+# 
 # # Assign ancestry for admixture output
 # tbl_proj2 <- tbl %>%
 #   mutate(super_pop2 = case_when((EUR > 0.85 & EAS < 0.1 & SAS < 0.1 & AFR <0.1 & AMR <0.1) ~ "EUR",
@@ -64,58 +99,92 @@ message("Reading pcs file \n")
 #         (AFR > 0.3 & EAS < 0.1 & SAS <0.1 & AFR > AMR) ~ "AFR",
 #         (AMR > 0.1 & EAS < 0.1 & SAS <0.1 ) ~ "AMR",
 #         TRUE~"Other"))
-#
+# 
 # message("Exporting tbl2 to ",projected_assign, "\n")
 # tbl_proj2 %>% write_tsv(projected_assign)
-
+# 
 
 # Interpreting supervised admixture output #
 ## ======================================##
 message("Reading supervised admixture output \n")
 tbl_super <- read_table(Qsuper,col_names = FALSE)
 
-message("Reading pop file \n")
-popfile <- read_table(popfile, col_names = FALSE)
-popfile <- unlist(popfile)
-
-message("Reading fam fixed file \n")
-famfile <- read_table(famfile, col_names = FALSE) %>%
- mutate(pop = popfile)
-
 message("Calculate the column mean, and assign the column with the max mean the ID \n")
 to_query <- tbl_super %>%
-  mutate(pop = popfile)
+  bind_cols(popfile)
 
 message("Assign column with max mean to that group \n")
-ind_oneKG=which(popfile!="-")
-oneKG=to_query[ind_oneKG,]
+oneKG = filter(to_query, super_pop != "-")
 
 # Get means for each pop, assign column with max mean to that group
 labels=rep(0,ncol(tbl_super))
-for (my_pop in unique(oneKG$pop)){
-  subset=oneKG[oneKG$pop==my_pop,]
+for (my_pop in unique(oneKG$super_pop)){
+  subset=oneKG[oneKG$super_pop==my_pop,]
   means=apply(subset[,colnames(tbl_super)],2,mean)
   print(means)
   ind=which(means==max(means))
   labels[ind]=my_pop
 }
-pop <- unlist(popfile)
-colnames(tbl_super)=labels
-tbl_super <- tbl_super %>%
-mutate(fid = fids, iid =ids, super_pop = super_pop) %>%
-filter(pop == "-")
 
-message("Exporting tbl to", unsupervised_interpret, "\n")
-tbl_super %>% write_tsv(unsupervised_interpret)
-
-# Assign ancestry for admixture output
-tbl_super2 <- tbl_super %>%
-  mutate(super_pop2 = case_when((EUR > 0.85 & EAS < 0.1 & SAS < 0.1 & AFR <0.1 & AMR <0.1) ~ "EUR",
+supervised_admixture <- tbl_super %>% 
+  magrittr::set_colnames(., labels) %>%
+  bind_cols(famfile) %>% 
+  relocate(fid, iid) %>%
+  mutate(admixture_super_pop = case_when((EUR > 0.85 & EAS < 0.1 & SAS < 0.1 & AFR <0.1 & AMR <0.1) ~ "EUR",
         (EAS > 0.51) ~ "EAS",
         (SAS > 0.51) ~ "SAS",
         (AFR > 0.3 & EAS < 0.1 & SAS <0.1 & AFR > AMR) ~ "AFR",
         (AMR > 0.1 & EAS < 0.1 & SAS <0.1 ) ~ "AMR",
         TRUE~"Other"))
 
-message("Exporting tbl2 to ",unsupervised_assign, "\n")
-tbl_super2 %>% write_tsv(unsupervised_assign)
+out <- pcs %>%
+  select(-FID) %>% 
+  left_join(supervised_admixture, by = c('fid', 'iid'))
+# 
+# out %>% 
+#   count(super_pop, pca_super_pop)
+out %>%
+  tabyl(admixture_super_pop, pca_super_pop)
+
+message("Exporting out to ", supervised_assign, "\n")
+out %>% write_tsv(supervised_assign)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- workflow/scripts/plot_admixture.R
+++ workflow/scripts/plot_admixture.R
@@ -1,23 +1,39 @@
 library(tidyverse)
 library(glue)
+
 sample <- "adni"; sample <- "dian"; sample <- "gsa1234567"
 
 q.path <- glue("sandbox/output/{sample}_1kG_merged.5.Q")
 pcs.path <- glue("sandbox/output/{sample}_1kG_pcs_pops.tsv")
 
-q.path <-  snakemake@input[['Q']]
-pcs.path <- snakemake@input[['pcs_pops']]
+q.path <-  snakemake@input[['Qdat2']]
+# pcs.path <- snakemake@input[['pcs_pops']]
+# 
+# Q.dat.raw <- read_table2(q.path, col_names = c("K1","K2","K3","K4","K5"))
+# pcs <- read_tsv(pcs.path)
 
-Q.dat.raw <- read_table2(q.path, col_names = c("K1","K2","K3","K4","K5"))
-pcs <- read_tsv(pcs.path)
-
-Q.dat <- bind_cols(pcs, Q.dat.raw)
+# Q.dat <- bind_cols(pcs, Q.dat.raw)
+Q.dat <- read_tsv(q.path)
 
 Q.dat.long <- Q.dat %>%
   filter(pop == 'sample') %>%
-  pivot_longer(c(K1,K2,K3,K4,K5), names_to = "K", values_to = "prop" ) %>%
-  mutate(K = fct_relevel(K, c("K4","K1","K2","K3","K5"))) %>%
+  pivot_longer(c("AFR", "SAS", "EAS", "EUR", "AMR"), names_to = "K", values_to = "prop" ) %>%
+  mutate(K = fct_relevel(K, c("EUR","AFR","AMR","EAS","SAS"))) %>%
   arrange(K, prop) %>%
+  mutate(iid = fct_inorder(iid))
+
+Q.dat.long <- Q.dat %>%
+  select(-starts_with("PC", ignore.case = F)) %>%
+  filter(pop == 'sample') %>%
+  rowwise(iid) %>% 
+  mutate( # pophelper::plotQ sortind == ALL 
+    maxval=max(c_across(c(AFR, SAS, EAS, EUR, AMR))), 
+    matchval=which.max(c_across(c(AFR, SAS, EAS, EUR, AMR))), 
+  ) %>% 
+  ungroup() %>% 
+  mutate(corder = order(matchval, -maxval)) %>%
+  pivot_longer(c("AFR", "SAS", "EAS", "EUR", "AMR"), names_to = "K", values_to = "prop" ) %>%
+  arrange(matchval, -maxval) %>%
   mutate(iid = fct_inorder(iid))
 
 plc <- c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3", "#FF7F00")
@@ -25,10 +41,10 @@ admix = ggplot(Q.dat.long , aes(x = iid, y = prop, fill = K)) +
   geom_bar(position="fill", stat="identity", width = 1) +
   scale_fill_manual(name="Super Population",
                     values=plc,
-                    breaks=c("K1","K5","K3","K4","K2") ,
+                    breaks=c("AFR","AMR","EAS","EUR","SAS") ,
                     labels=c("AFR", "AMR","EAS", "EUR", "SAS")) +
   theme_classic() + labs(x = "Indivuals", y = "Global Ancestry", color ="Super Population") +
-  facet_grid(~fct_inorder(super_pop), switch = "x", scales = "free", space = "free")+
+  facet_grid(~fct_relevel(pca_super_pop, "EUR", "AFR", "AMR", "EAS", "SAS"), switch = "x", scales = "free", space = "free")+
   theme(
     axis.text.x = element_blank(),
     axis.ticks.x=element_blank(),
